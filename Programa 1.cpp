8a7cec09b0066dd043fa08a7eec02ef859025acd
GonzaloTobajas <113891014+GonzaloTobajas@users.noreply.github.com>
2023-01-16 09:32:13 +0000

    Pending changes exported from your codespace

---
+++ Programa 1.cpp
@@ -0,0 +1,13 @@
+//Instrucciones básicas
+#include<iostream>//Librería de cabecera
+using namespace std;
+
+int main(){
+    //definimos 3 variables
+    int x,y,z;
+    cout<<"introduce tres variables: ";
+    cin>> x >> y >> z;
+    cout<<"Has introducido las variables: ";
+    cout<< x <<"," <<y <<"," << z << endl;
+    return 0;
+}

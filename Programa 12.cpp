eacf66bea3e530931435d9b68f45254d632b8ec9
GonzaloTobajas <113891014+GonzaloTobajas@users.noreply.github.com>
2023-02-10 09:05:44 +0000

    Pending changes exported from your codespace

---
+++ Programa 12.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+//Genera una matriz de orden tres con números del 0 al 9
+using namespace std;
+int main(){
+    semillita
+    srand();
+    return 0;
+}
---
+++ Triangulo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Escribe un número -->  ";
+  cin >> n;
+  
+  for (int i = 1; i <= n; i++) {
+    for (int j = n; j > i; j--) {
+      cout << " ";
+    }
+    for (int k = 1; k <= 2 * i - 1; k++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+  return 0;
+}
